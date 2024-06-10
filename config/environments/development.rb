4034e5c6f0a5232281ec2afbbccacc98fc71c17e
Anil Kumar <anni@Anils-MacBook-Air.local>
2024-06-10 14:18:20 +0530

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow connections to local server on cloud IDE. 
+  config.hosts.clear
 end
