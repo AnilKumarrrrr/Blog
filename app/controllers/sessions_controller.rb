c007f3e5802718b6de8afce0eea318098cce308f
Anil Kumar <anni@Anils-MacBook-Air.local>
2024-06-11 16:19:28 +0530

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) 
 
     if user && user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to forwarding_url || user
-      
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end 
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-
 # Create a main sample user.
 User.create!(
-  name: "Example User",
-  email: "example@railstutorial.org",
+  name: "Anil",
+  email: "akb200101@gmail.com",
   password: "foobar",
-  password_confirmation: "foobar"
-  admin: true)
+  password_confirmation: "foobar",
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # Generate a bunch of additional users.
@@ -27,6 +18,8 @@ User.create!(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
