3f6467bc0639983517ef6fdb962b35d48b044b0e
Anil Kumar <anni@Anils-MacBook-Air.local>
2024-06-18 17:34:12 +0530

    Fixed account activation link issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,17 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
+    @user.activation_token = User.new_token
+    @user.update_attribute(:activation_digest, User.digest(@user.activation_token))
     mail to: user.email, subject: "Account activation"
   end
 
 
   def password_reset(user)
     @user = user
+    @user.reset_token = User.new_token
+    @user.update_attribute(:reset_digest, User.digest(@user.reset_token))
     mail to: user.email, subject: "Password reset"
   end
-
 
 end
