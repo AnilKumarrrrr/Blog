49c13f5e61dfdc12706bb41f2477fe0d051807a4
Anil Kumar <anni@Anils-MacBook-Air.local>
2024-06-18 17:50:28 +0530

    generalize account activation and password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    user = User.last
+    user = User.find_by(id: params[:user_id]) || User.last
     user.activation_token = User.new_token
     user.update_attribute(:activation_digest, User.digest(user.activation_token))
     UserMailer.account_activation(user)
@@ -11,7 +11,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    user = User.last
+    user = User.find_by(id: params[:user_id]) || User.last
     user.reset_token = User.new_token
     user.update_attribute(:reset_digest, User.digest(user.reset_token))
     UserMailer.password_reset(user)
